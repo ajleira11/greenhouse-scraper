f141821c7b0b43a3d54943e3c172639f4b6fe71d
Ariel Joseph Amano <arieljosephamano@gmailcom>
2024-02-14 17:55:38 +1300

    add a dockerfile and index.js for cloud run

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const puppeteer = require("puppeteer");
+const { Storage } = require("@google-cloud/storage");
 
-async function main() {
+async function initBrowser() {
+  console.log("Initializing browser");
+  return await puppeteer.launch();
+}
+
+async function getAllJobs(browser, url) {
   try {
-    const inputURL = "https://boards.greenhouse.io/neara";
-    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     console.log("Launching Browser...");
 
-    await page.goto(inputURL); //visit the webpage coming from the input
+    await page.goto(url); //visit the webpage coming from the input
     await page.waitForSelector(".level-0"); // Wait for the job listings to load to ensure data exists
 
     const companyName = await page.$eval(
@@ -60,9 +64,7 @@ async function main() {
     console.log(`manage to get the company/work details`);
 
     const finalJob = await description(tempJobs, browser);
-    console.log(finalJob);
-
-    await browser.close();
+    return finalJob;
   } catch (e) {
     console.error(e);
   }
@@ -111,4 +113,75 @@ async function description(jobs, browser) {
   }
 }
 
-main();
+async function createStorageBucketIfMissing(storage, bucketName) {
+  console.log(
+    `Checking for Cloud Storage bucket '${bucketName}' and creating if not found`
+  );
+  const bucket = storage.bucket(bucketName);
+  const [exists] = await bucket.exists();
+  if (exists) {
+    // Bucket exists, nothing to do here
+    return bucket;
+  }
+
+  // Create bucket
+  const [createdBucket] = await storage.createBucket(bucketName);
+  console.log(`Created Cloud Storage bucket '${createdBucket.name}'`);
+  return createdBucket;
+}
+
+async function uploadImage(bucket, taskIndex, allJobs) {
+  // Create filename using the current time and task index
+  const date = new Date();
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  const filename = `${date.toISOString()}-${
+    jobsData[0].companyName
+  }-task${taskIndex}`;
+
+  console.log(`Uploading screenshot as '${filename}'`);
+  await bucket.file(filename).save(allJobs);
+
+  const finalData = JSON.stringify(jsonData);
+  console.log(`Uploading data as '${filename}.json'`);
+  await bucket.file(`${filename}.json`).save(finalData);
+}
+
+async function main(urls) {
+  console.log(`Passed in urls: ${urls}`);
+
+  const taskIndex = process.env.CLOUD_RUN_TASK_INDEX || 0;
+  const url = urls[taskIndex];
+  if (!url) {
+    throw new Error(
+      `No url found for task ${taskIndex}. Ensure at least ${
+        parseInt(taskIndex, 10) + 1
+      } url(s) have been specified as command args.`
+    );
+  }
+  const bucketName = process.env.BUCKET_NAME;
+  if (!bucketName) {
+    throw new Error(
+      "No bucket name specified. Set the BUCKET_NAME env var to specify which Cloud Storage bucket the screenshot will be uploaded to."
+    );
+  }
+
+  const browser = await initBrowser();
+  const allJobs = await getAllJobs(browser, url).catch(async (err) => {
+    // Make sure to close the browser if we hit an error.
+    await browser.close();
+    throw err;
+  });
+  await browser.close();
+
+  console.log("Initializing Cloud Storage client");
+  const storage = new Storage();
+  const bucket = await createStorageBucketIfMissing(storage, bucketName);
+  await uploadImage(bucket, taskIndex, allJobs);
+
+  console.log("Upload complete!");
+}
+
+main(process.argv.slice(2)).catch((err) => {
+  console.error(JSON.stringify({ severity: "ERROR", message: err.message }));
+  process.exit(1);
+});
